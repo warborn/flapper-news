6134eeea15114e16b4566d8e9bd52efb8c66109d
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2016-04-14 04:40:56 +0000

    Add home and post states with ui router, Add posts controller and implement comments for each post

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 'use strict';
-angular.module('flapperNews', []);
+angular.module('flapperNews', ['ui.router'])
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    }).
+    state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
 
 (function() {
-  var mainCtrl = function($scope) {
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 10},
-      {title: 'post 4', upvotes: 1},
-      {title: 'post 5', upvotes: 8},
-      {title: 'post 6', upvotes: 3}
-    ];
+  var mainCtrl = function($scope, posts) {
+    $scope.posts = posts.posts;
 
     $scope.addPost = function(title) {
       if(!$scope.title || $scope.title === '') { return; }
       $scope.posts.push({
         title: $scope.title,
+        link: $scope.link,
         upvotes: 0,
-        link: $scope.link
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
       });
       $scope.title = "";
       $scope.link = "";
@@ -28,8 +40,37 @@ angular.module('flapperNews', []);
     }
   }
 
-  mainCtrl.$inject = ['$scope'];
+  mainCtrl.$inject = ['$scope', 'posts'];
+
+  var postsCtrl = function($scope, $stateParams, posts) {
+    $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function() {
+      if(!$scope.body || $scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    }
+  }
+
+  postsCtrl.$inject = ['$scope', '$stateParams', 'posts'];
+
+  var postsFactory = function() {
+    return {
+      posts: [{
+        title: "Angular Rails",
+        link: "https://thinkster.io/angular-rails",
+        upvotes: 0,
+        comments: []
+      }]
+    };
+  }
 
   angular.module('flapperNews')
-    .controller('MainCtrl', mainCtrl);
+    .controller('MainCtrl', mainCtrl)
+    .controller('PostsCtrl', postsCtrl)
+    .factory('posts', postsFactory)
 })();
