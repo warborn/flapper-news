7153f4c75a200825f40e1e6fe2b3072bb35d9f42
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2016-04-14 02:04:23 +0000

    Getting user input for posts

--- app.js
+++ app.js
@@ -3,7 +3,29 @@ angular.module('flapperNews', []);
 
 (function() {
   var mainCtrl = function($scope) {
-    $scope.test = 'Hello world!';
+    $scope.posts = [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 10},
+      {title: 'post 4', upvotes: 1},
+      {title: 'post 5', upvotes: 8},
+      {title: 'post 6', upvotes: 3}
+    ];
+
+    $scope.addPost = function(title) {
+      if(!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({
+        title: $scope.title,
+        upvotes: 0,
+        link: $scope.link
+      });
+      $scope.title = "";
+      $scope.link = "";
+    }
+
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    }
   }
 
   mainCtrl.$inject = ['$scope'];
